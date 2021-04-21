e289199d8b373614273fbc950c54451794f30e4a
David Konsumer <konsumer@jetboystudio.com>
2021-04-21 00:54:48 -0700

    quiet server

--- Makefile
+++ Makefile
@@ -17,22 +17,15 @@ clean: ## delete all output files
 build: ## build runtime files for current system in target/release
 	# whatever is local
 	cargo build --bins --lib --release
-	cbindgen . -o target/release/rattata.h --lang C
-
-ffi: ## test lua ffi wrappers
-	cargo build --lib
-	LD_LIBRARY_PATH=target/debug/ luajit src/example.lua
 
 # group release builder for posix systems
 x86_64-unknown-linux-gnu x86_64-apple-darwin arm-unknown-linux-gnueabihf armv7-unknown-linux-gnueabihf:
 	cross build --bins --lib --release --target=$@
-	cbindgen . -o target/$@/release/rattata.h --lang C
 	cd target/$@/release/ && zip $@.zip *.lua runtime manager librattata.so rattata.h && mv $@.zip ../../
 
 # single release builder for windows
 x86_64-pc-windows-gnu:
 	cross build --bins --lib --release --target=$@
-	cbindgen . -o target/$@/release/rattata.h --lang C
 	cd target/$@/release/ && zip $@.zip *.lua runtime.exe manager.exe librattata.dll rattata.h && mv $@.zip ../../
 
 release: x86_64-unknown-linux-gnu x86_64-apple-darwin arm-unknown-linux-gnueabihf armv7-unknown-linux-gnueabihf x86_64-pc-windows-gnu ## build runtime files for all supported platforms in target/releases
--- src/manager/rattata.rs
+++ src/manager/rattata.rs
@@ -1,57 +1,27 @@
-use libtor::{ Tor, TorFlag, HiddenServiceVersion, TorAddress, log, Error };
 use dirs;
-use std::fs::{ File };
-use std::io::{ Read, Write };
-use std::ffi::CString;
-use std::net::{ TcpListener, TcpStream, SocketAddr };
-use std::thread::{ spawn, JoinHandle };
-use std::fmt::{ format };
+use libtor::{log, Error, HiddenServiceVersion, Tor, TorAddress, TorFlag};
 use portpicker::pick_unused_port;
 
+use std::fmt::format;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::thread::{spawn, JoinHandle};
+
 // this makes write work on socket
-#[allow(unused)] use std::io::prelude::*;
-
-// FFI/C interface
-
-#[cfg(target_pointer_width = "32")]
-#[no_mangle]
-pub extern "C" fn ffi_location() -> *const u8 {
-    return CString::new(location()).unwrap().into_raw();
-}
-
-#[cfg(target_pointer_width = "32")]
-#[no_mangle]
-pub extern "C" fn ffi_hostname() -> *const u8 {
-    return  CString::new(hostname()).unwrap().into_raw();
-}
-
-#[cfg(target_pointer_width = "64")]
-#[no_mangle]
-pub extern "C" fn ffi_location() -> *const i8 {
-    return CString::new(location()).unwrap().into_raw();
-}
-
-#[cfg(target_pointer_width = "64")]
-#[no_mangle]
-pub extern "C" fn ffi_hostname() -> *const i8 {
-    return  CString::new(hostname()).unwrap().into_raw();
-}
-
-#[no_mangle]
-pub extern "C" fn ffi_start(port: u16) -> u16 {
-    let (_socket, tor, setport) = start(port);
-    let _ = tor.join();
-    return setport;
-}
-
-#[no_mangle]
-pub extern "C" fn ffi_choose_port() -> u16 {
-    return pick_unused_port().unwrap();
-}
+#[allow(unused)]
+use std::io::prelude::*;
 
 /// get the current setings dir (which has tor stuff in it)
 pub fn location() -> String {
-    return format!("{}/rattata", dirs::config_dir().unwrap().into_os_string().into_string().unwrap());
+    return format!(
+        "{}/rattata",
+        dirs::config_dir()
+            .unwrap()
+            .into_os_string()
+            .into_string()
+            .unwrap()
+    );
 }
 
 /// handle an incoming request
@@ -59,7 +29,14 @@ fn handle_client(mut stream: TcpStream) {
     // TODO: actual server here
     println!("connect");
     let content = "<h1>Howdy, Hacker!</h1>";
-    let _ = stream.write(format(format_args!("HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: {}\nConnection: close\n\n{}", content.len(), content)).as_bytes());
+    let _ = stream.write(
+        format(format_args!(
+            "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: {}\nConnection: close\n\n{}",
+            content.len(),
+            content
+        ))
+        .as_bytes(),
+    );
 }
 
 /// start a tor server & the local service connected to it
@@ -71,7 +48,11 @@ pub fn start(mut port: u16) -> (JoinHandle<()>, JoinHandle<Result<u8, Error>>, u
         .flag(TorFlag::SocksPort(0))
         .flag(TorFlag::HiddenServiceDir(location()))
         .flag(TorFlag::HiddenServiceVersion(HiddenServiceVersion::V3))
-        .flag(TorFlag::HiddenServicePort(TorAddress::Port(port), None.into()))
+        .flag(TorFlag::Custom("--quiet".into()))
+        .flag(TorFlag::HiddenServicePort(
+            TorAddress::Port(port),
+            None.into(),
+        ))
         .flag(TorFlag::Log(log::LogLevel::Err))
         .start_background();
 
